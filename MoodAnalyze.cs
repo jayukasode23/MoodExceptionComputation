04e12e0db27053d067595203ee5ac714b969d10a
jayukasode23 <jayukasode2398@gmail.com>
2022-10-22 18:32:16 +0530

    Added Ability To Respond Happy Or Sad

---
+++ MoodAnalyze.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyze
+    {
+        private string message;
+        public MoodAnalyze(string message)
+        {
+            this.message = message;
+        }
+        //Analysing the mood by passing happy Or sad.
+        public string AnalyseMood(string message)
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+
+        }
+    }
+}
