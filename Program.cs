7ad459bb14a7aefd512a53adc8ead8793d12283d
jayukasode23 <jayukasode2398@gmail.com>
2022-10-22 18:30:08 +0530

    Added Mood Analyzer

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MoodAnalyzerProblem;
+static void Main(string[] args)
+{
+    Console.WriteLine(":::::Welcome To Mood Analyzer Program:::::::");
+    Console.WriteLine("***Please Enter Which Mood You Have E.g- Happy Or Sad***");
+    string message = Console.ReadLine();
+    // Create Instance Of A class
+    MoodAnalyze check = new MoodAnalyze(message);
+
+    //referance is stored in result
+    string result = check.AnalyseMood(message);
+    Console.WriteLine(result + "Mood");
+
+
+
+
+
+}
